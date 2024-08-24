b046a8026157d356c2ae191c2495199f9bb19fdb
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-08-24 12:43:00 -0300

    FEAT: Criação do projeto com algumas das dependências necessárias

---
+++ src/main/java/com/vinimanfrin/toys_fiap/ToysFiapApplication.java
@@ -0,0 +1,13 @@
+package com.vinimanfrin.toys_fiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysFiapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToysFiapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinimanfrin/toys_fiap/ToysFiapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinimanfrin.toys_fiap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToysFiapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
